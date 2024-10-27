8f2ad3285a1dd4d708cd431d28ea46db1bd31d71
Luis Carlos Rodriguez <luisc.rodriguezpacheco@gmail.com>
2024-10-27 00:34:56 -0600

    asignando firma

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,12 +1,12 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { OrderItem, MenuItem } from "../types"
 
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
-    const addItem = () => {
-        console.log("agregando...")
+    const addItem = (item : MenuItem) => {
+        console.log(item)
     }
 
   return {
