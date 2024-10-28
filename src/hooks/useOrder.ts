5db5d1d270d872e907230b3a3b9b9100dcbbaa92
Luis Carlos Rodriguez <luisc.rodriguezpacheco@gmail.com>
2024-10-27 22:20:58 -0600

    order y set order usado

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,8 +6,17 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([])
 
     const addItem = (item : MenuItem) => {
-        console.log(item)
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if (itemExist){
+            console.log("ya existe")
+        } else {
+            const newItem : OrderItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
+        console.log(order)
     }
+
+     
 
   return {
     addItem
