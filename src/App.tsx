268e8ef8531f16e4a34ceac1c64edc5ae770504b
Luis Carlos Rodriguez <luisc.rodriguezpacheco@gmail.com>
2024-11-12 22:45:18 -0600

    use reducer first commit

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           </div>
         </div>
         <div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
-          {order.length === 0 ? (
+          {order.length > 0 ? (
             <>
               <OrderContents
                 order={order}
